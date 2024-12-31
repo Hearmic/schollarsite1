3a1970a1ff5e74305cbe705a6ed257b103610328
Grigoriy Adamenko <hearmic@gmail.com>
2024-12-31 18:31:12 +0500

    calender folder deleted

--- KnowledgeBase/migrations/0001_initial.py
+++ KnowledgeBase/migrations/0001_initial.py
@@ -26,7 +26,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=200)),
-                ('description', models.TextField(blank=True, null=True)),
+                ('description', models.TextField(blank=True)),
             ],
         ),
         migrations.CreateModel(
@@ -42,10 +42,10 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=255)),
-                ('description', models.TextField(blank=True, null=True)),
+                ('description', models.TextField(blank=True)),
                 ('upload_date', models.DateTimeField(auto_now_add=True)),
                 ('files', models.FileField(blank=True, null=True, upload_to='materials/files/')),
-                ('link', models.URLField(blank=True, null=True)),
+                ('link', models.URLField(blank=True)),
                 ('views', models.IntegerField(blank=True, null=True)),
                 ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
--- calender/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from calender import views
-
-app_name = 'calender'
-
-urlpatterns = [
-    path('',views.calender, name='calender_main')
-]
