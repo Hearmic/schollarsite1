4cd40967025907bb9f7571621f15f2fb71fd5ebd
Hearmic <hearmic@gmail.com>
2025-01-03 13:20:58 +0500

    CHANGE: linting using super-linter

--- canteen_menu/apps.py
+++ canteen_menu/apps.py
@@ -1,6 +1,8 @@
+"""Этот модуль определяет конфигурацию приложения canteen_menu"""
 from django.apps import AppConfig
 
 
 class CanteenMenuConfig(AppConfig):
+    """Конфигурация приложения canteen_menu"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'canteen_menu'
