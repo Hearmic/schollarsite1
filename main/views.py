202e162e6c955c11647a6b7cd37515e2924938fa
Hearmic <hearmic@gmail.com>
2025-01-03 11:52:46 +0500

    UPDATE: news linter-suggested adjustments

--- main/views.py
+++ main/views.py
@@ -5,16 +5,17 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-    context: dict[str,Any]={  # нужна для передачи данных в .html шаблон
-    'title': 'Home',
-    'teachers':  ['Учитель', 'Классный руководитель', 'Школьная администрация'],
-    'admin': ['Системный администратор']
+    context: dict[str, Any] = {  # нужна для передачи данных в .html шаблон
+        'title': 'Home',
+        'teachers':  ['Учитель', 'Классный руководитель', 'Школьная администрация'],
+        'admin': ['Системный администратор']
     }
     return render(request, 'main/index.html', context)  # возвращаем рендер .html файла в папке /templates(поиск тут по дефолту)/main/index.html; заполняем данные шаблона переменной 'context'
 
 
 def teachers_list(request):
     return render(request, 'main/teachers_list.html')
+
 
 def design_system(request):
     return render(request, 'main/design_system.html')
--- news/views.py
+++ news/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def main(request):
     context = {
-        
     }
-    return render(request,'news/main.html', context)
+    return render(request, 'news/main.html', context)
